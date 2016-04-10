bfc1c1959bfaca3b666921873aafce3e79054682
mattsteinpreis <matt@nexosis.com>
2016-04-10 15:30:38 -0400

    added auto_arima to forecasting

--- Contests/RealData_April2016/forecasting_passenger_traffic.py
+++ Contests/RealData_April2016/forecasting_passenger_traffic.py
@@ -1,4 +1,25 @@
 import matplotlib.pyplot as plt
+from statsmodels.tsa.arima_model import ARIMA
+from scipy.optimize import brute
+
+
+def auto_arima(ts):
+
+    def objfunc(order, x):
+        mod = ARIMA(x, order)
+        try:
+            mod = mod.fit(disp=0)
+        except ValueError:
+            return 1e6
+        return mod.aic
+
+    grid = (slice(0, 3, 1), slice(0, 3, 1), slice(0, 3, 1))
+    res = brute(objfunc, grid, args=[ts], finish=None)
+    best_order = [int(i) for i in res]
+    best_model = ARIMA(ts, best_order).fit(disp=0)
+    pred = best_model.predict(60, 71, typ='levels')
+    return pred
+
 
 def test():
     f = open(u'D:/PythonProjects/HackerRank/Contests/RealData_April2016/traffic.txt', 'rb')
@@ -9,9 +30,16 @@ def test():
         case = raw[i+1].split()
         val = float(case[1])
         targets.append(val)
-    mn = sum(targets) / len(targets)
-    for i in range(12):
-        print(int(mn))
+
+    # # mean
+    # mn = sum(targets) / len(targets)
+    # for i in range(12):
+    #     print(int(mn))
+
+    predictions = auto_arima(targets)
+    print(predictions)
+
+
 
 def hacker_rank():
     n_cases = int(input())
@@ -20,9 +48,9 @@ def hacker_rank():
         case = input().split()
         val = float(case[1])
         targets.append(val)
-    mn = sum(targets) / len(targets)
-    for i in range(12):
-        print(int(mn))
+    predictions = auto_arima(targets)
+    for pred in predictions:
+        print(int(pred))
 
 if __name__ == "__main__":
     hacker_rank()
