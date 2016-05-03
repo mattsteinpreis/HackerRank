a1e06ddadd2845401ef90e18a8f1a59b6572dfb9
mattsteinpreis <matt@nexosis.com>
2016-05-03 10:17:11 -0400

    added additional word type functionality

--- Domains/Regex/british_american_spelling.py
+++ Domains/Regex/british_american_spelling.py
@@ -6,7 +6,12 @@ def n_finds(p, s):
 
 
 def find_all_words(w, s):
-    pattern = r'\b'+w[:-2]+r'[sz]e\b'
+    if w.endswith('ze'):
+        pattern = r'\b'+w[:-2]+r'[sz]e\b'
+    else:
+        sub_p = r'^([a-z]+)our([a-z]*)$'
+        m = re.match(sub_p, w)
+        pattern = r'\b'+m.group(1)+r'ou?r'+m.group(2)+r'\b'
     return n_finds(pattern, s)
 
 
@@ -14,10 +19,20 @@ def test():
     lines = ['hackerrank has such a good ui that it takes no time to familiarise its environment',
              'to familiarize oneself with ui of hackerrank is easy']
     w = 'familiarize'
-
     s = ' '.join(lines)
     f = find_all_words(w, s)
-    print(f)
+    assert f == 2
+
+    lines = ['the odour coming out of the left over food was intolerable',
+             'ammonia has a very pungent odor']
+    w = 'odour'
+    s = ' '.join(lines)
+    assert find_all_words(w, s) == 2
+
+    # lines = ['labour however salty access stream strange odor favorite dancing milligram',
+    #          'anxious spoon formal lesson vapor close soft drunk odour pt',
+    #          'text labor instead knit shop flavor find humor critical driving']
+    # words = []
 
 
 def main():
