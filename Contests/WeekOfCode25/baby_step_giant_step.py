c9a123ac4b4028c6058ffc69403e025db47db3db
mattsteinpreis <matt.steinpreis@gmail.com>
2016-11-05 20:50:22 -0400

    enforced integer answer; added test function

--- Contests/WeekOfCode25/baby_step_giant_step.py
+++ Contests/WeekOfCode25/baby_step_giant_step.py
@@ -1,16 +1,27 @@
-n_queries = int(input())
-
 def perform(a, b, d):
     if d == 0:
         return 0
     if d % b == 0:
-        return d / b
+        return d // b
     if d > b:
         return (d // b) + 1
     if d == a:
         return 1
     return 2
 
-for i in range(n_queries):
-    a, b, d = [int(item) for item in input().split()]
-    print(perform(a, b, d))
+
+def test():
+    assert perform(1, 2, 6) == 3
+    assert perform(1, 2, 1000000000) == 500000000
+    assert perform(1, 5, 5) == 1
+
+
+def HackerRank():
+    n_queries = int(input())
+    for i in range(n_queries):
+        a, b, d = [int(item) for item in input().split()]
+        print(perform(a, b, d))
+
+
+if __name__ == '__main__':
+    HackerRank()
